7a105581ff1c04582dceb1b77fd746bbdb058289
Sahal <117257225+m-sahal1@users.noreply.github.com>
2023-08-02 12:49:09 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.py
@@ -0,0 +1,82 @@
+#User function Template for python3
+
+'''
+Structure of node
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+'''
+
+#Function to find the length of a loop in the linked list.
+def countNodesinLoop(head):
+    if head==None:
+        return 0
+    fast=head.next
+    slow=head
+    while(fast!=slow):
+        if(fast==None or fast.next==None or fast.next.next==None or slow.next==None or slow ==None):
+            return 0
+        fast=fast.next.next
+        slow=slow.next
+    
+    count=0
+    while(slow.next!=fast):
+        count+=1
+        slow=slow.next
+    return count+1
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+    
+    #connects last node to node at position pos from begining.
+    def loopHere(self,pos):
+        if pos==0:
+            return
+        
+        walk = self.head
+        for i in range(1,pos):
+            walk = walk.next
+        
+        self.tail.next = walk
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        
+        LL = LinkedList()
+        for i in input().split():
+            LL.insert(int(i))
+        
+        LL.loopHere(int(input()))
+        
+        print(countNodesinLoop(LL.head))
+
+# } Driver Code Ends
